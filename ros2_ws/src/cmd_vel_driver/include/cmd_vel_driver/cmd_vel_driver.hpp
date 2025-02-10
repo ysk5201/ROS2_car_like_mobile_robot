3a02c2e7e804ecb8f63e594022da4286cbd3625c
ysk5201 <yskknd1025@gmail.com>
2025-02-10 11:54:20 +0900

    modify steering input calculation

--- ros2_ws/src/cmd_vel_driver/include/cmd_vel_driver/cmd_vel_driver.hpp
+++ ros2_ws/src/cmd_vel_driver/include/cmd_vel_driver/cmd_vel_driver.hpp
@@ -19,13 +19,11 @@ public:
     ~CmdVelDriver();
 
     // Member functions
-    void calcCommand(double dt);
+    void calcCommand();
     void publishCommand();
 
 private:
     // constants
-    // static constexpr int STATE_VARIABLE_DIM = 4;   // 車両型移動ロボットの状態変数
-    // static constexpr int RUNGE_DIM = 4;            // Runge()で計算する状態変数の数
     static constexpr double PI = 3.141592653589793;
     static constexpr double WHEEL_BASE = 1.0;      // 車軸間距離(m) (URDFと統一)
     static constexpr double TREAD_WIDTH = 0.856;   // 左右車輪間距離(m) (URDFと統一)
@@ -34,7 +32,6 @@ private:
     // Member Variables
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
 
-    double pre_time_;
     double linear_velocity_, angular_z_;
 
     double fl_steering_angle_, fr_steering_angle_;
