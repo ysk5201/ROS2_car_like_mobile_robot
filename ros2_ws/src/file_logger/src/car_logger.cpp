1f2424a95335c5975640075a375bf50359d2217c
tamashu <shu5.tmr@gmail.com>
2025-02-10 11:22:29 +0900

    modify car_logger to add date to filename

--- ros2_ws/src/file_logger/src/car_logger.cpp
+++ ros2_ws/src/file_logger/src/car_logger.cpp
@@ -1,6 +1,7 @@
 #include "file_logger/car_logger.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include <filesystem>
+#include <chrono>
 
 DataLogger::DataLogger() 
   : Node("car_logger")
@@ -10,16 +11,30 @@ DataLogger::DataLogger()
   std::filesystem::path package_dir = std::filesystem::path(package_directory).parent_path().parent_path();
   std::string log_dir = package_dir.string() + "/log_files/";
 
-  // /state_variable トピックのサブスクライバー作成
+  //現在の時間をYYMMDDHHMMSS形式に変換
+  auto now = std::chrono::system_clock::now();
+  auto now_time = std::chrono::system_clock::to_time_t(now);
+  std::stringstream ss;
+  ss << std::put_time(std::localtime(&now_time), "%Y%m%d_%H%M%S");
+  std::string time_str = ss.str();
+
+  //ファイル名を作成
+  std::string file_name = log_dir + "debug_log_" + time_str + ".csv";
+
+  // ファイルを新規作成（上書き）モードでオープン
+  data_file_.open(file_name, std::ios::out | std::ios::trunc);
+  if (!data_file_.is_open()) {
+    RCLCPP_ERROR(this->get_logger(), "Failed to open data_log.txt");
+  }
+
+
+  // debug_info トピックのサブスクライバー作成
   debug_info_sub_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
     "/debug_info", 10,
     std::bind(&DataLogger::debugInfoCallback, this, std::placeholders::_1)
   );
-  // ファイルを追記モードでオープン
-  data_file_.open(log_dir+OUTPUT_FILE_NAME_, std::ios::out | std::ios::app);
-  if (!data_file_.is_open()) {
-    RCLCPP_ERROR(this->get_logger(), "Failed to open data_log.txt");
-  }
+
+  
 }
 
 DataLogger::~DataLogger()
@@ -31,18 +46,13 @@ DataLogger::~DataLogger()
 
 void DataLogger::debugInfoCallback(const std_msgs::msg::Float64MultiArray::SharedPtr msg)
 {
-  // state変数は [0]: x, [1]: y, [2]: theta, [3]: phi が必要
-  if (msg->data.size() < 6) {
-    RCLCPP_WARN(this->get_logger(), "Received state_variable message with insufficient data.");
-    return;
-  }
-  
-  data_file_  << msg->data[0] << ", "   //x
-              << msg->data[1] << ", "   //y
-              << msg->data[2] << ", "   //theta
-              << msg->data[3] << ", "   //phi
-              << msg->data[4] << ", "   //u1
-              << msg->data[5] << "\n";  //u2
+  int msg_size = msg->data.size();
+
+  for (int i = 0; i < msg_size -1; i++) {
+    data_file_  << msg->data[i] << ", " ;
+  } 
+  data_file_<< msg->data[msg_size-1] << "\n";
+
   data_file_.flush();
 }
 
